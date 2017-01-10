fe1a206880626231e3d6ec31cbe88f93bb2e403a
Henrik Larsson <henrik.larsson@leogears.com>
2017-01-10 15:00:08 +0100

    Toolbox sass (#2)
    * Added yarn lock file
    
    * Fixed scss loading of react toolbox

--- app/App.jsx
+++ app/App.jsx
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import { Button } from 'react-toolbox/lib/button';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import {Button} from 'react-toolbox/lib/button';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-       <h1>React is awesome!</h1>
-        <Button label='Hello World!' />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>React is awesome!</h1>
+                <Button label='Hello World!' raised primary/>
+            </div>
+        );
+    }
 }
 
 render(
-  <App />,
-  document.getElementById('app')
+    <App />,
+    document.getElementById('app')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -4,68 +4,68 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
-  
-  entry: './app/',
-  context: __dirname,
 
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
+    entry: './app/',
+    context: __dirname,
 
-      {
-        test: /\.css$/,
-        loader: ['style-loader', 'css-loader?sourceMap', 'postcss-loader',]
-      },
-      { test: /\.scss$/, 
-          loader: ExtractTextPlugin.extract({
-            fallbackLoader: "style-loader",
-            loader: ['css-loader', 'sass-loader']
-        }) 
-      },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
 
-      {
-        test: /\.html$/,
-        exclude: /node_modules/,
-        loader: [
-            'file-loader?name=[name].[ext]',
-            'extract-loader',
-            'html-loader'
+            {
+                test: /\.css$/,
+                loader: ['style-loader', 'css-loader?sourceMap', 'postcss-loader',]
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader',
+                    'css-loader?sourceMap&-minimize&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+                    'postcss-loader',
+                    'sass-loader?sourceMap']
+            },
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                loader: [
+                    'file-loader?name=[name].[ext]',
+                    'extract-loader',
+                    'html-loader'
+                ]
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                loader: 'file-loader?name=fonts/roboto/[name].[ext]'
+            }
         ]
-      },
-        {
-            test: /\.(eot|svg|ttf|woff|woff2)$/,
-            loader: 'file-loader?name=fonts/roboto/[name].[ext]'
-        }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.css'],
-    modules: [
-      'node_modules',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css', '.scss'],
+        modules: [
+            'node_modules',
+        ],
+    },
+
+    plugins: [
+        new ExtractTextPlugin({
+            filename: '/style/[name].css',
+            allChunks: true,
+        }),
     ],
-  },
 
-  plugins: [
-      new ExtractTextPlugin({
-        filename: '/style/[name].css',
-        allChunks: true,
-      }),
-  ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 8081
+    },
 
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 8081
-  },
-  
-  devtool: 'source-map'
+    devtool: 'source-map'
 
 };
