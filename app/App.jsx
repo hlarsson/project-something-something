1f62bfe53181176ab0ecd8241722ca51c53c8d6b
Sebastian <sebastian.lindstrom@gmail.com>
2017-01-10 15:56:07 +0100

    Added basic layout (#4)

--- app/App.jsx
+++ app/App.jsx
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import {Button} from 'react-toolbox/lib/button';
+import AppBar from 'react-toolbox/lib/app_bar';
+import Navigation from 'react-toolbox/lib/navigation';
+import Link from 'react-toolbox/lib/Link';
+import appStyle from './app.scss';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <h1>React is awesome!</h1>
-                <Button label='Hello World!' raised primary/>
+            <div className={appStyle.mainWrapper}>
+                <AppBar title="React Toolbox">
+                    <Navigation type="horizontal">
+                    <Link href="http://" label="Inbox" icon="inbox" />
+                    <Link href="http://" active label="Profile" icon="person" />
+                    </Navigation>
+                </AppBar>
+                <div className={appStyle.contentWrapper}>
+                    <main className={appStyle.mainContent}>
+                        Main content
+                    </main>
+                    <aside className={appStyle.rightContent}>
+                        Right content
+                    </aside>
+                </div>
+                <footer>…</footer>
             </div>
         );
     }
