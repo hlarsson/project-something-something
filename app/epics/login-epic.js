7babd6e518a56273f7f7bad75d34673028d02fe6
Henrik Larsson <henrik.larsson@leogears.com>
2017-01-20 14:15:06 +0100

    Login flow fix (#8)
    * Fixed login action creator
    
    * Added missing deps

--- app/epics/login-epic.js
+++ app/epics/login-epic.js
@@ -3,16 +3,16 @@ import { userLoginSuccess, userLoginFailure } from '../actions/creators'
 import { ajax } from 'rxjs/observable/dom/ajax';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mapTo';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/observable/of';
 
 export default action$ => 
     action$.ofType(USER_LOGIN_REQUEST)
-        .do(console.log)
+      .do(console.log)
         .mergeMap(action =>
             ajax.getJSON('/login')
-                .map(response => userLoginSuccess)
+                .mapTo(userLoginSuccess())
                 .catch(error => Observable.of(userLoginFailure(error)))
             );
